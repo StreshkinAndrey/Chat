9fdc457ebeaa3faf6813fb26fc331248179bd0b5
andrey <andrey.streshkin.96@mail.ru>
2022-05-26 15:35:41 +0300

    HM3.3 done

--- src/main/java/ru/gb/gbchat1/Controller.java
+++ src/main/java/ru/gb/gbchat1/Controller.java
@@ -1,5 +1,8 @@
 package ru.gb.gbchat1;
 
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import javafx.event.ActionEvent;
@@ -33,11 +36,12 @@ public class Controller {
 
     private final ChatClient client;
 
-    public Controller() {
+    public Controller() throws IOException {
         client = new ChatClient(this);
         while (true) {
             try {
                 client.openConnection();
+                loadHistory();
                 break;
             } catch (Exception e) {
                 showNotification();
@@ -68,7 +72,7 @@ public class Controller {
         messageBox.setVisible(success);
     }
 
-    private void showNotification() {
+    private void showNotification() throws IOException {
         final Alert alert = new Alert(Alert.AlertType.ERROR,
                 "Не могу подключится к серверу.\n" +
                         "Проверьте, что сервер запущен",
@@ -78,6 +82,7 @@ public class Controller {
         final Optional<ButtonType> buttonType = alert.showAndWait();
         final Boolean isExit = buttonType.map(btn -> btn.getButtonData().isCancelButton()).orElse(false);
         if (isExit) {
+           SaveHistory();
             System.exit(0);
         }
     }
@@ -101,5 +106,46 @@ public class Controller {
     public void updateClientList(String[] params) {
         clientList.getItems().clear();
         clientList.getItems().addAll(params);
+    }
+
+    private void SaveHistory() throws IOException {
+        try {
+            File history = new File("history.txt");
+            if (!history.exists()) {
+                System.out.println("Файла истории нет,создадим его");
+                history.createNewFile();
+            }
+            PrintWriter fileWriter = new PrintWriter(new FileWriter(history, false));
+
+            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+            bufferedWriter.write(textArea.getText());
+            bufferedWriter.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void loadHistory() throws IOException {
+        int posHistory = 100;
+        File history = new File("history.txt");
+        List<String> historyList = new ArrayList<>();
+        FileInputStream in = new FileInputStream(history);
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(in));
+
+        String temp;
+        while ((temp = bufferedReader.readLine()) != null) {
+            historyList.add(temp);
+        }
+
+        if (historyList.size() > posHistory) {
+            for (int i = historyList.size() - posHistory; i <= (historyList.size() - 1); i++) {
+                textArea.appendText(historyList.get(i) + "\n");
+            }
+        } else {
+            for (int i = 0; i < posHistory; i++) {
+                System.out.println(historyList.get(i));
+            }
+        }
     }
 }
