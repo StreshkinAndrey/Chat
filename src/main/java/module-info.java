0d2d875e69b74fb453209be09957e2faf237cd06
andrey <andrey.streshkin.96@mail.ru>
2022-05-28 16:13:05 +0300

    HM3.6 done

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,9 @@ module ru.gb.gbchat1 {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
+    requires org.apache.logging.log4j;
+    requires org.testng;
+    requires junit;
 
 
     opens ru.gb.gbchat1 to javafx.fxml;
--- src/main/java/ru/gb/gbchat1/server/ChatServer.java
+++ src/main/java/ru/gb/gbchat1/server/ChatServer.java
@@ -1,5 +1,8 @@
 package ru.gb.gbchat1.server;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -8,9 +11,12 @@ import java.util.Map;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+
 import ru.gb.gbchat1.Command;
 
 public class ChatServer {
+
+    private static final Logger logger = LogManager.getLogger(ChatServer.class);
 
     private final Map<String, ClientHandler> clients;
 
@@ -23,13 +29,15 @@ public class ChatServer {
         try (ServerSocket serverSocket = new ServerSocket(8189);
              AuthService authService = new InMemoryAuthService()) {
             while (true) {
-                System.out.println("Wait client connection...");
+                logger.debug("Сервер запускается...");
+                logger.info("Wait client connection...");
                 final Socket socket = serverSocket.accept();
                 new ClientHandler(socket, this, authService, executorService);
-                System.out.println("Client connected");
+                logger.info("Client connected");
             }
         } catch (IOException e) {
             e.printStackTrace();
+            logger.error(e.getMessage());
         } finally {
             executorService.shutdownNow();
         }
@@ -77,6 +85,7 @@ public class ChatServer {
             sender.sendMessage("участнику " + to + ": " + message);
         } else {
             sender.sendMessage(Command.ERROR, "Участника с ником " + to + " нет в чате!");
+            logger.debug("Участника с ником " + to + " нет в чате!");
         }
     }
 }
